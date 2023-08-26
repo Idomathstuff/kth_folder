b0948df5a27d1c1827b991acd64162196290d886
Lucas Frykman <l.m.frykman@gmail.com>
2023-08-26 17:52:24 +0200

    commit

--- kth_scripts/DD1385Jav/tmp1_jav/tmproj/bin/movies.sql
+++
@@ -1,1 +0,0 @@
-SELECT AVG(rating) AS average_rating FROM movies;
--- kth_scripts/DD1385Jav/tmp1_jav/tmproj/src/tmp.java
+++
@@ -1,8 +0,0 @@
-public class tmp {
-    static int A = 1729;
-    static String B = "blah";  
-    public static void main(String[] args){
-        System.out.println(B);
-    }   
-}
-
