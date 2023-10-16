9bf6d74f6b4b994867354f198e938a3603db80e5
Lucas <l.m.frykman@gmail.com>
2023-10-16 21:03:20 +0200

    I gotta solve this projekt bruh

--- kth_scripts/projekt_ode/inlämning/uppgift1.py
+++ kth_scripts/projekt_ode/inlämning/uppgift1.py
@@ -26,11 +26,9 @@ def euler(step_size, f, starty, startx, stopx):
     y = [starty]
     for k in range(n):
         next_x = x[-1]+dx
-        next_y = y[-1]+ dx*f(x[-1],y[-1])
+        next_y = y[-1]+dx*f(x[-1],y[-1])
         x.append(next_x)
         y.append(next_y)
-    # for a, b in zip(x, y):
-    #     print(round(a, 2), round(b, 2))
     return x,y
 
 
@@ -52,17 +50,43 @@ def runge_kutta(step_size, f, start_y, start_x, stop_x):
         y.append(next_y)
     return x,y
 
-x1,y1 = runge_kutta(0.01, f, -9/4, 0, 5)
-x2,y2 = runge_kutta(0.1, f, -1, -1, 5)
+x1, y1 = euler(0.011, f, -1, -1, 5)
+def get_interval(x_values,y_values):
+    n_steps = len(x_values)
+    for i in range(1,n_steps):
+        if y_values[i-1]<0 and y_values[i]>0:
+            lower_bound = x_values[i-1]
+            upper_bound = x_values[i]
+            return lower_bound, upper_bound
 
-plt.grid(True)
-# plt.plot(x1, y1)
-plt.scatter(x2,y2, marker=".", label=r"numerisk lösning, $h=0.1$")
-plt.plot(x2,[y(np.array(x),-1) for x in x2],color="orange", label=r"analytisk lösning")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-plt.show()
+def plot_interval_against_step_size():
+    step_sizes = [2**(-i) for i in range(1,12)]
+    interval_lengths = []
+    for step_size in step_sizes:
+        x_values, y_values = euler(step_size, f, -1, -1, 5)
+        lower_bound, upper_bound = get_interval(x_values, y_values)
+        interval_lengths.append(upper_bound - lower_bound)
+
+    # Convert step sizes and interval lengths to logarithmic scale
+    log_step_sizes = [np.log2(step) for step in step_sizes]
+    log_interval_lengths = [np.log2(length) for length in interval_lengths]
+
+    # Create a log-log plot
+    plt.scatter(log_step_sizes, log_interval_lengths)
+    plt.xlabel('Log Step Size (log2)')
+    plt.ylabel('Log Interval Length (log2)')
+    plt.show()
+plot_interval_against_step_size()
+
+# print(get_interval(x1,y1))
+
+# plt.grid(True)
+# plt.scatter(x1,y1, marker=".", label=r"numerisk lösning, $h=0.1$")
+# plt.plot(x1,[y(np.array(x),-1) for x in x1],color="orange", label=r"analytisk lösning")
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.legend()
+# plt.show()
 
 
 
